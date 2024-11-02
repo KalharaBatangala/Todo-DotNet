b00646a53747af311334ec92d7946b7d075369a1
Kalhara Batangala <kalharabatangala@gmail.com>
2024-11-02 19:55:08 +0530

    Update repo-test

--- obj/Release/net8.0/ToDoApp.AssemblyInfo.cs
+++ obj/Release/net8.0/ToDoApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d884e36c9e3b1cfb45897183a17b1cd8c31689b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
